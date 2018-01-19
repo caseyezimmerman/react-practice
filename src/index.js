5a789b9c1ce1cc0633e552b751d3f0e11ad2299f
Casey Zimmerman <caseyezimmerman@gmail.com>
2018-01-19 10:01:13 -0500

    simple passing props

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Props from './passingProps'
+
+
+ReactDOM.render(
+	<Props name='Casey' />, 
+	document.getElementById('root')
+);
+
